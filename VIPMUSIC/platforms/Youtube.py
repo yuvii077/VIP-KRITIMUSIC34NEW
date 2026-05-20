fcd84772f7306736ababd116249fabb0942740ef
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-05-20 18:54:19 +0530

    Update API URL for YouTube platform

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -27,7 +27,7 @@ class SensitiveDataFilter(logging.Filter):
 logging.getLogger().addFilter(SensitiveDataFilter())
 
 # Note: Agar ye API band hai toh yt-dlp automatically handle karega
-API_URL = "https://vip-bot.up.railway.app"
+API_URL = "https://youtube-mini.up.railway.app/"
 
 # --- UTILS ---
 def get_clean_id(link: str) -> Optional[str]:
